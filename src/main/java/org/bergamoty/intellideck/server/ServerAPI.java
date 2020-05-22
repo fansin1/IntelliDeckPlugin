ff32e45a556e96bac094140826335c7672d8b7ea
kostiamatv <kostiamatv@gmail.com>
2020-05-22 17:05:59 +0300

    create ServerAPI service

--- src/main/java/org/bergamoty/intellideck/server/ServerAPI.java
+++
@@ -1,9 +0,0 @@
-package org.bergamoty.intellideck.server;
-
-import com.intellij.openapi.components.ServiceManager;
-
-public interface ServerAPI {
-    static ServerAPI getInstance() {
-        return ServiceManager.getService(ServerAPI.class);
-    }
-}
---
+++ src/main/java/org/bergamoty/intellideck/server/ServerAPIServiceImpl.java
@@ -0,0 +1,5 @@
+package org.bergamoty.intellideck.server;
+
+public class ServerAPIServiceImpl implements ServerAPIService {
+
+}
