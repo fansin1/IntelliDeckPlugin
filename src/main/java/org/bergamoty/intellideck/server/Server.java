91737ec0b742054e02a007165ad9bd428a71b36f
volyomaS <rastoden@gmail.com>
2020-05-22 18:27:27 +0300

    add Server.java for thread

---
+++ src/main/java/org/bergamoty/intellideck/server/Server.java
@@ -0,0 +1,19 @@
+package org.bergamoty.intellideck.server;
+
+import com.intellij.openapi.components.ServiceManager;
+
+import java.io.IOException;
+
+public class Server implements Runnable {
+    @Override
+    public void run() {
+        try {
+            ServerAPIServiceImpl serverService = ServiceManager.getService(ServerAPIServiceImpl.class);
+            while (serverService.client.isClosed()) {
+                String entry = serverService.in.readUTF();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
