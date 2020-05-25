90dd199b4f4599c8d16ce03e70bc3927346c127a
kostiamatv <kostiamatv@gmail.com>
2020-05-25 18:08:30 +0300

    fixed cold configuration run

--- src/main/java/org/bergamoty/intellideck/plugin/RunCommand.java
+++ src/main/java/org/bergamoty/intellideck/plugin/RunCommand.java
@@ -1,12 +1,15 @@
 package org.bergamoty.intellideck.plugin;
 
 
-import com.intellij.execution.ProgramRunnerUtil;
-import com.intellij.execution.RunManager;
+import com.intellij.execution.*;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.execution.runners.ExecutionUtil;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Objects;
 
 
@@ -28,8 +31,25 @@ public class RunCommand implements Command {
 
     @Override
     public void run() {
-        ProgramRunnerUtil.executeConfiguration(Objects.requireNonNull(runManager.findSettings(runConfiguration)),
-                DefaultRunExecutor.getRunExecutorInstance());
+
+        RunnerAndConfigurationSettings runConfig = runManager.findConfigurationByName(runConfiguration.getName());
+        Executor executor = DefaultRunExecutor.getRunExecutorInstance();
+        ExecutionTarget target = getExecutionTarget(runConfiguration.getProject(), Objects.requireNonNull(runConfig));
+        ExecutionUtil.runConfiguration(runConfig, executor, target);
+    }
+
+    @NotNull
+    private ExecutionTarget getExecutionTarget(@NotNull Project project, @NotNull RunnerAndConfigurationSettings runConfig) {
+        ExecutionTargetManager targetManager = ExecutionTargetManager.getInstance(project);
+        ExecutionTarget active = targetManager.getActiveTarget();
+
+        List<ExecutionTarget> targets = targetManager.getTargetsFor(runConfig.getConfiguration());
+        for (ExecutionTarget target : targets) {
+            if (target.getId().equals(DefaultRunExecutor.getRunExecutorInstance().getId())) {
+                return target;
+            }
+        }
+        return active;
     }
 
 }
